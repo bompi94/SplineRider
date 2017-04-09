3d73f2cbeef74b337a9f32a2c3cb8c3f0d935407
Antonio Ruggiero <antonio.ruggiero93@hotmail.it>
2017-04-09 13:50:34 +0200

    Simple start game Countdown

---
+++ Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField]
+	Image countDownImage;
+
+	[SerializeField]
+	Sprite[] countDownNumbers;
+
+	private bool inCountDown=false;
+	private bool gameStarted=false;
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+		countDownImage.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gameStarted) {
+			if (Input.GetKeyDown (KeyCode.Return)) {
+				if (!inCountDown) {
+					inCountDown = true;
+					StartCoroutine (StartCountdown());
+				}
+			}
+		}
+	}
+
+	IEnumerator StartCountdown(){
+		countDownImage.enabled = true;
+		Time.timeScale = 0;
+		for(int i=0;i<countDownNumbers.Length;i++){
+			countDownImage.sprite = countDownNumbers [i];
+			yield return new WaitForSecondsRealtime (1);
+		}
+		countDownImage.enabled = false;
+		Time.timeScale = 1;
+		inCountDown = false;
+		gameStarted = true;
+	}
+}
