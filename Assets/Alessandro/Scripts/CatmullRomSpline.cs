148501b2da3a559ab88a138023d7050168a4cf2f
Alessandro Buonpane <ale.buonpane@gmail.com>
2017-04-10 11:06:12 +0200

    project reorganized + some minor changes

---
+++ Assets/Alessandro/Scripts/CatmullRomSpline.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//Interpolation between points with a Catmull-Rom spline
+public class CatmullRomSpline : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCollider;  
+
+    //the lower the value, the smoother the line
+    //1/resolution represents the number of intermediate points there will be between the knots of the spline
+    public float resolution;
+
+    List<Vector3> positions = new List<Vector3>();
+
+    public void Draw(Transform[] knots)
+    { 
+        positions.Clear();
+        //Draw the Catmull-Rom spline between the points for each pair on points
+        for (int i = 0; i < knots.Length; i++)
+        {
+            //avoids getting out of range
+            if (i == 0 || i - 1 < 0 || i + 1 > knots.Length - 1 || i + 2 > knots.Length - 1)
+            {
+                continue;
+            }
+            else
+            {
+                Vector3 pi_minus_1 = knots[i - 1].position;
+                Vector3 pi = knots[i].position;
+                Vector3 pi_plus_1 = knots[i + 1].position;
+                Vector3 pi_plus_2 = knots[i + 2].position;
+
+                DrawCatmullRomSpline(pi_minus_1, pi, pi_plus_1, pi_plus_2);
+            }
+        }
+    }
+
+    //draws a spline between pi and pi_plus_1
+    void DrawCatmullRomSpline(Vector3 pi_minus_1, Vector3 pi, Vector3 pi_plus_1, Vector3 pi_plus_2)
+    {
+        positions.Add(pi); 
+        
+        //how many intermediate points should we get? 
+        int numberOfIntermediatePoints = Mathf.FloorToInt(1f / resolution);
+
+        for (int i = 1; i < numberOfIntermediatePoints; i++)
+        {
+            float percentageOnTheLine = i * resolution;
+            Vector3 intermediatePoint = GetCatmullRomPosition(percentageOnTheLine, pi_minus_1, pi, pi_plus_1, pi_plus_2);
+            positions.Add(intermediatePoint);
+        }
+
+        positions.Add(pi_plus_1);
+
+        //draw line in unity game
+        lineRenderer.numPositions = positions.Count;
+        lineRenderer.SetPositions(positions.ToArray());
+        //create collider for the line
+        Vector2[] temp = new Vector2[positions.Count];
+        for (int i = 0; i < positions.Count; i++)
+        {
+            temp[i] = new Vector2(positions[i].x, positions[i].y); 
+        }
+        edgeCollider.points = temp;
+    }
+
+    Vector3 GetCatmullRomPosition(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        //coefficients
+        Vector3 a = 2f * p1;
+        Vector3 b = p2 - p0;
+        Vector3 c = 2f * p0 - 5f * p1 + 4f * p2 - p3;
+        Vector3 d = -p0 + 3f * p1 - 3f * p2 + p3;
+
+        //The cubic polynomial: a + b * t + c * t^2 + d * t^3
+        Vector3 pos = 0.5f * (a + (b * t) + (c * t * t) + (d * t * t * t));
+
+        return pos;
+    }
+}
---
+++ Assets/Alessandro/Scripts/LineDrawController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDrawController : MonoBehaviour
+{
+
+    public Color color; 
+    public GameObject splinePrefab;
+    public GameObject pointPrefab;
+    public int pointsNeededToDraw = 4;
+
+    GameObject mySplineObject;
+    int counter = 0;
+    List<Transform> pointsTransforms = new List<Transform>();
+    CatmullRomSpline spline; 
+
+    private void Awake()
+    {
+        mySplineObject = Instantiate(splinePrefab);
+        mySplineObject.GetComponent<LineRenderer>().startColor = color;
+        mySplineObject.GetComponent<LineRenderer>().endColor = color;
+        spline = mySplineObject.GetComponent<CatmullRomSpline>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.timeScale!=0)
+        {
+            counter++;
+            AddPointToShow();
+            if (counter == pointsNeededToDraw)
+            {
+                ResetOldStuff();
+               
+                Transform[] points = pointsTransforms.ToArray(); 
+                spline.Draw(points);
+                mySplineObject.SetActive(true); 
+
+                ClearAllPoints();
+            }
+
+        }
+    }
+
+    void ResetOldStuff()
+    {
+        mySplineObject.SetActive(false); 
+        counter = 0; 
+    }
+
+    void AddPointToShow()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GameObject pointClone = Instantiate(pointPrefab);
+        pointClone.GetComponent<SpriteRenderer>().color = color; 
+        pointClone.transform.position = mousePos;
+        pointsTransforms.Add(pointClone.transform);
+
+        //I will add 2 point at the extremes because they are the control points
+        if (counter == 1 || counter == pointsNeededToDraw) 
+            pointsTransforms.Add(pointClone.transform);
+    }
+
+    void ClearAllPoints()
+    {
+        foreach (Transform t in pointsTransforms)
+        {
+            Destroy(t.gameObject);
+        }
+        pointsTransforms.Clear();
+    }
+}
