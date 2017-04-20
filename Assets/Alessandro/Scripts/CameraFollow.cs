59768c3ba4d42fe45af53b4ac74b7bafee736d87
Alessandro Buonpane <ale.buonpane@gmail.com>
2017-04-20 10:10:24 +0200

    added difficulty choice

--- Assets/Alessandro/Scripts/CameraFollow.cs
+++ Assets/Alessandro/Scripts/CameraFollow.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-    public GameObject target;
     public float speed; 
 
-	// Update is called once per frame
 	void Update () {
-        //transform.position = new Vector3(target.transform.position.x, transform.position.y, transform.position.z); 
 		speed=GameDifficultyUtility.Instance.getCameraSpeed();
         transform.position += new Vector3(speed, 0,0) * Time.deltaTime; 
 	}
--- Assets/Antonio/Scripts/GameDifficultyUtility.cs
+++ Assets/Antonio/Scripts/GameDifficultyUtility.cs
@@ -3,69 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class GameDifficultyUtility : MonoBehaviour {
-	
-	public static GameDifficultyUtility Instance;
+public class GameDifficultyUtility : MonoBehaviour
+{
 
-	[SerializeField]
-	private float DifficultyUpdateInterval=1f;
+    public static GameDifficultyUtility Instance;
 
-	[SerializeField]
-	private float SurfaceEffectorMinSpeed=3f;
+    private float surfaceEffectorSpeedBasic = 4;
+    private float cameraSpeedBasic = 3;
 
-	[SerializeField]
-	private float SurfaceEffectorMaxSpeed=300f;
+    public bool challenge;
 
-	[SerializeField]
-	private float SurfaceEffectorIncrementPercentage=5f;
+    [SerializeField]
+    private float DifficultyUpdateInterval = 1f;
 
-	[SerializeField]
-	private float CameraMinSpeed=4f;
+    [SerializeField]
+    private float SurfaceEffectorMinSpeed = 3f;
 
-	[SerializeField]
-	private float CameraMaxSpeed=300f;
+    [SerializeField]
+    private float SurfaceEffectorMaxSpeed = 300f;
 
-	[SerializeField]
-	private float CameraSpeedIncrementPercentage=5f;
+    [SerializeField]
+    private float SurfaceEffectorIncrementPercentage = 5f;
 
-	[SerializeField]
-	private float currentCameraSpeed;
-	[SerializeField]
-	private float currentSurfaceEffectorSpeed;
+    [SerializeField]
+    private float CameraMinSpeed = 4f;
+
+    [SerializeField]
+    private float CameraMaxSpeed = 300f;
+
+    [SerializeField]
+    private float CameraSpeedIncrementPercentage = 5f;
+
+    [SerializeField]
+    private float currentCameraSpeed;
+    [SerializeField]
+    private float currentSurfaceEffectorSpeed;
 
 
-	void Awake(){
-		if (Instance == null) {
-			Instance = this;
-		} else {
-			Destroy (gameObject);		
-		}
-	}
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-		currentCameraSpeed = CameraMinSpeed;
-		currentSurfaceEffectorSpeed = SurfaceEffectorMinSpeed;
-		AdjustDifficulty ();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        currentCameraSpeed = CameraMinSpeed;
+        currentSurfaceEffectorSpeed = SurfaceEffectorMinSpeed;
+        if (challenge)
+            AdjustDifficulty();
+    }
 
-	public void AdjustDifficulty(){
-		//calcolo il 5% del valore attuale, quindi incremento il valore attuale di quella quantità
-		float tmp = (currentCameraSpeed * CameraSpeedIncrementPercentage) / 100;
-		currentCameraSpeed += tmp;
+    public void AdjustDifficulty()
+    {
+        //calcolo il 5% del valore attuale, quindi incremento il valore attuale di quella quantità
+        float tmp = (currentCameraSpeed * CameraSpeedIncrementPercentage) / 100;
+        currentCameraSpeed += tmp;
 
-		//calcolo il 5% del valore attuale, quindi incremento il valore attuale di quella quantità
-		tmp = (currentSurfaceEffectorSpeed * SurfaceEffectorIncrementPercentage) / 100;
-		currentSurfaceEffectorSpeed += tmp;
+        //calcolo il 5% del valore attuale, quindi incremento il valore attuale di quella quantità
+        tmp = (currentSurfaceEffectorSpeed * SurfaceEffectorIncrementPercentage) / 100;
+        currentSurfaceEffectorSpeed += tmp;
 
-		Invoke ("AdjustDifficulty", DifficultyUpdateInterval);
-	}
+        Invoke("AdjustDifficulty", DifficultyUpdateInterval);
+    }
 
-	public float getCameraSpeed(){
-		return Mathf.Clamp (currentCameraSpeed, CameraMinSpeed, CameraMaxSpeed);
-	}
+    public float getCameraSpeed()
+    {
+        if (challenge)
+            return Mathf.Clamp(currentCameraSpeed, CameraMinSpeed, CameraMaxSpeed);
+        return cameraSpeedBasic;
+    }
 
-	public float getSurfaceEffectorForce(){
-		return Mathf.Clamp (currentSurfaceEffectorSpeed, SurfaceEffectorMinSpeed,SurfaceEffectorMaxSpeed);
-	}
+    public float getSurfaceEffectorForce()
+    {
+        if (challenge)
+            return Mathf.Clamp(currentSurfaceEffectorSpeed, SurfaceEffectorMinSpeed, SurfaceEffectorMaxSpeed);
+        return surfaceEffectorSpeedBasic;
+    }
 }
