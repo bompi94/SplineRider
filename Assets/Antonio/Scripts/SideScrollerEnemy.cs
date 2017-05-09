ed154ada93847c166f355eabf023f95e756dc071
Alessandro Buonpane <ale.buonpane@gmail.com>
2017-05-09 13:39:28 +0200

    audio imported

--- Assets/Antonio/Scripts/SideScrollerEnemy.cs
+++ Assets/Antonio/Scripts/SideScrollerEnemy.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SideScrollerEnemy : SideScrollerObject {
-    string playerTag = "Player"; 
+    string playerTag = "Player";
+    string hit = "Hit"; 
 	void OnTriggerEnter2D(Collider2D coll){
 		if (coll.CompareTag (playerTag)) {
 			GameManager.Instance.OnPlayerLosesLife.Invoke ();
             gameObject.SetActive(false); 
-			coll.GetComponent<Animator> ().SetTrigger ("Hit");
+			coll.GetComponent<Animator> ().SetTrigger (hit);
 		}
 	}
 }
